5d6075163343acc887521d23488ec7ace500106e
ghost <ghostintheboot@protonmail.com>
2021-07-31 17:48:02 -0500

    pre Username removal

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -26,8 +26,8 @@ const Login = ({ history }) => {
     try {
       // Destructure data from axios request.
       const { data } = await axios.post(
-        "/api/auth/login", 
-        { email, password }, 
+        "/api/auth/login",
+        { email, password },
         config
       );
 
@@ -47,41 +47,41 @@ const Login = ({ history }) => {
   return (
     <div>
       <h1 className="login-screen__calculator-title">CALCULATR</h1>
-    <div className="login-screen">
-      <form onSubmit={loginHandler} className="login-screen__form">
-        <h3 className="login-screen__title">Login</h3>
-        {error && <span className="error-message">{error}</span>}
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input 
-            type="email"
-            required
-            id="email"
-            placeholder="Email address"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            tabIndex={1}
-          />
-        </div>
+      <div className="login-screen">
+        <form onSubmit={loginHandler} className="login-screen__form">
+          <h3 className="login-screen__title">Login</h3>
+          {error && <span className="error-message">{error}</span>}
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              required
+              id="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              tabIndex={1}
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="password">Password: {""}
-            <Link 
-              to="/forgotpassword" 
-              className="login-screen__forgotpassword"
-              tabIndex={4}
-            >Forgot Your Password?</Link>
-          </label>
-          <input 
-            type="password"
-            required
-            id="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            tabIndex={2}
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: {""}
+              <Link
+                to="/forgotpassword"
+                className="login-screen__forgotpassword"
+                tabIndex={4}
+              >Forgot Your Password?</Link>
+            </label>
+            <input
+              type="password"
+              required
+              id="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              tabIndex={2}
+            />
+          </div>
 
           <button type="submit" className="btn btn-primary" tabIndex={3}>
             Login To Calculatr
@@ -91,8 +91,8 @@ const Login = ({ history }) => {
             Need an account? <Link to="/register">Register here.</Link>
           </span>
 
-      </form>
-    </div>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/components/screens/Register.js
+++ client/src/components/screens/Register.js
@@ -1,4 +1,3 @@
-/*1️⃣8️⃣*/
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -29,7 +28,7 @@ const Register = ({ history }) => {
     if (password !== confirmPassword) {
       setPassword("");
       setConfirmPassword("");
-      setTimeout( () => {
+      setTimeout(() => {
         setError("");
       }, 4000);
       return setError("Passwords do not match.");
@@ -37,9 +36,9 @@ const Register = ({ history }) => {
 
     try {
       // Destructure data from axios request.
-      const {data} = await axios.post(
-        "/api/auth/register", 
-        {username, email, password}, 
+      const { data } = await axios.post(
+        "/api/auth/register",
+        { username, email, password },
         config
       );
 
@@ -50,74 +49,77 @@ const Register = ({ history }) => {
     } catch (error) {
       // Axios gives an error, then a response, then data, then the error we created here.
       setError(error.response.data.error);
-      setTimeout( () => {
+      setTimeout(() => {
         setError("");
       }, 4000);
     }
   }
 
   return (
-    <div className="register-screen">
-      <form onSubmit={registerHandler} className="register-screen__form">
-        <h3 className="register-screen__title">Register</h3>
-        {error && <span className="error-message">{error}</span>}
-        <div className="form-group">
-          <label htmlFor="name">Username:</label>
-          <input 
-            type="text"
-            required
-            id="name"
-            placeholder="Enter username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
+    <div>
+      <h1 className="register-screen__calculator-title">CALCULATR</h1>
+      <div className="register-screen">
+        <form onSubmit={registerHandler} className="register-screen__form">
+          <h3 className="register-screen__title">Register</h3>
+          {error && <span className="error-message">{error}</span>}
+          <div className="form-group">
+            <label htmlFor="name">Username:</label>
+            <input
+              type="text"
+              required
+              id="name"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input 
-            type="email"
-            required
-            id="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              required
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input 
-            type="password"
-            required
-            id="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              required
+              id="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input 
-            type="password"
-            required
-            id="confirmPassword"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              required
+              id="confirmPassword"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary">
-          Register
-        </button>
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
 
-        <span className="register-screen__subtext">
-          Already have an account? <Link to="/login">Login here.</Link>
-        </span>
+          <span className="register-screen__subtext">
+            Already have an account? <Link to="/login">Login here.</Link>
+          </span>
 
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
